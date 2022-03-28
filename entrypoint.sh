a06ea95b50f5cbe837dafeffb1cd5e2627257999
Alexandra Østemark <alex.cramt@gmail.com>
2022-03-28 15:12:32 +0200

    wow, spelling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p $INPUT_PORT:8125 statd/statd"
+docker_run="$docker_run -d -p $INPUT_PORT:8125 statsd/statsd"
 
 sh -c "$docker_run"
